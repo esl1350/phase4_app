a94052f73da36d30cabbc1cba855454d5f2c5030
Elizabeth Liu <esl1350@gmail.com>
2022-12-04 17:06:38 -0500

    display owner view, landing
    oops im pushing to main

--- app.py
+++ app.py
@@ -10,10 +10,13 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'cs4400#CS4400%'
 app.config['MYSQL_DB'] = 'restaurant_supply_express'
-@app.route('/', methods=['POST', 'GET'])
 
-
+@app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/addUser', methods = ['GET', 'POST'])
+def addUser():
     alert = ''
     if request.method == 'POST':
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
@@ -25,11 +28,17 @@ def index():
         if len(username) <= 40 and len(fname) <= 100 and len(lname) <= 100 and len(address) <= 500 and len(birthdate) == 10:
             cursor.execute('call add_owner(% s, % s, % s, % s, % s)', (username, fname, lname, address, birthdate))
             mysql.connection.commit()
-
-            alert = 'success!'
+            alert = 'query executed!'
         else:
             alert = 'field lengths incorrect'
-    return render_template('index.html', alert = alert)
+    return render_template('addUser.html', alert = alert)
 
+@app.route('/displayOwner')
+def displayOwner():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM display_owner_view;')
+    rows = cursor.fetchall()
+    print(rows)
+    return render_template('displayOwner.html', rows = rows)
 if __name__ == "__main__":
     app.run(debug=True)
