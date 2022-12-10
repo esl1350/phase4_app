7f5df3823c6ea66c6914f3b9e5b6e1538161b612
Khoang4 <kenny.m.hoang@gmail.com>
2022-12-10 16:51:06 -0500

    employee view. setup section 5 and 6

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ mysql = MySQL(app)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'cs4400#CS4400%'
+app.config['MYSQL_PASSWORD'] = 'Simba@316'
 app.config['MYSQL_DB'] = 'restaurant_supply_express'
 
 @app.route('/')
@@ -38,7 +38,106 @@ def displayOwner():
     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
     cursor.execute('SELECT * FROM display_owner_view;')
     rows = cursor.fetchall()
-    print(rows)
     return render_template('displayOwner.html', rows = rows)
+
+@app.route('/flyDrone', methods = ['GET', 'POST'])
+def flyDrone():
+    alert = ''
+    if request.method == 'POST':
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        username = request.form['username']
+        fname = request.form['fname']
+        lname = request.form['lname']
+        address = request.form['address']
+        birthdate = request.form['birthdate']
+        if len(username) <= 40 and len(fname) <= 100 and len(lname) <= 100 and len(address) <= 500 and len(birthdate) == 10:
+            cursor.execute('call add_owner(% s, % s, % s, % s, % s)', (username, fname, lname, address, birthdate))
+            mysql.connection.commit()
+            alert = 'query executed!'
+        else:
+            alert = 'field lengths incorrect'
+    return render_template('addUser.html', alert = alert)
+
+@app.route('/joinSwarm', methods = ['GET', 'POST'])
+def joinSwarm():
+    alert = ''
+    if request.method == 'POST':
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        username = request.form['username']
+        fname = request.form['fname']
+        lname = request.form['lname']
+        address = request.form['address']
+        birthdate = request.form['birthdate']
+        if len(username) <= 40 and len(fname) <= 100 and len(lname) <= 100 and len(address) <= 500 and len(birthdate) == 10:
+            cursor.execute('call add_owner(% s, % s, % s, % s, % s)', (username, fname, lname, address, birthdate))
+            mysql.connection.commit()
+            alert = 'query executed!'
+        else:
+            alert = 'field lengths incorrect'
+    return render_template('addUser.html', alert = alert)
+
+@app.route('/leaveSwarm', methods = ['GET', 'POST'])
+def leaveSwarm():
+    alert = ''
+    if request.method == 'POST':
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        username = request.form['username']
+        fname = request.form['fname']
+        lname = request.form['lname']
+        address = request.form['address']
+        birthdate = request.form['birthdate']
+        if len(username) <= 40 and len(fname) <= 100 and len(lname) <= 100 and len(address) <= 500 and len(birthdate) == 10:
+            cursor.execute('call add_owner(% s, % s, % s, % s, % s)', (username, fname, lname, address, birthdate))
+            mysql.connection.commit()
+            alert = 'query executed!'
+        else:
+            alert = 'field lengths incorrect'
+    return render_template('addUser.html', alert = alert)
+
+@app.route('/startFunding', methods = ['GET', 'POST'])
+def startFunding():
+    alert = ''
+    if request.method == 'POST':
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        username = request.form['username']
+        fname = request.form['fname']
+        lname = request.form['lname']
+        address = request.form['address']
+        birthdate = request.form['birthdate']
+        if len(username) <= 40 and len(fname) <= 100 and len(lname) <= 100 and len(address) <= 500 and len(birthdate) == 10:
+            cursor.execute('call add_owner(% s, % s, % s, % s, % s)', (username, fname, lname, address, birthdate))
+            mysql.connection.commit()
+            alert = 'query executed!'
+        else:
+            alert = 'field lengths incorrect'
+    return render_template('addUser.html', alert = alert)
+
+@app.route('/takeoverDrone', methods = ['GET', 'POST'])
+def takeoverDrone():
+    alert = ''
+    if request.method == 'POST':
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        username = request.form['username']
+        fname = request.form['fname']
+        lname = request.form['lname']
+        address = request.form['address']
+        birthdate = request.form['birthdate']
+        if len(username) <= 40 and len(fname) <= 100 and len(lname) <= 100 and len(address) <= 500 and len(birthdate) == 10:
+            cursor.execute('call add_owner(% s, % s, % s, % s, % s)', (username, fname, lname, address, birthdate))
+            mysql.connection.commit()
+            alert = 'query executed!'
+        else:
+            alert = 'field lengths incorrect'
+    return render_template('addUser.html', alert = alert)
+
+@app.route('/displayEmployee')
+def displayEmployee():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM display_employee_view;')
+    rows = cursor.fetchall()
+    return render_template('displayEmployee.html', rows = rows)
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
