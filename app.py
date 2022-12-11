46049e7966cfa3770ad14a5f463168321348646e
Khoang4 <kenny.m.hoang@gmail.com>
2022-12-10 23:50:17 -0500

    Sections 5 and 6

--- app.py
+++ app.py
@@ -45,72 +45,62 @@ def flyDrone():
     alert = ''
     if request.method == 'POST':
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        username = request.form['username']
-        fname = request.form['fname']
-        lname = request.form['lname']
-        address = request.form['address']
-        birthdate = request.form['birthdate']
-        if len(username) <= 40 and len(fname) <= 100 and len(lname) <= 100 and len(address) <= 500 and len(birthdate) == 10:
-            cursor.execute('call add_owner(% s, % s, % s, % s, % s)', (username, fname, lname, address, birthdate))
+        ip_id = request.form['ip_id']
+        ip_tag = request.form['ip_tag']
+        ip_destination = request.form['ip_destination']
+        if len(ip_id) <= 40 and len(ip_destination) <= 10:
+            cursor.execute('call fly_drone(% s, % d, % s)', (ip_id, ip_tag, ip_destination))
             mysql.connection.commit()
             alert = 'query executed!'
         else:
             alert = 'field lengths incorrect'
-    return render_template('addUser.html', alert = alert)
+    return render_template('flyDrone.html', alert = alert)
 
 @app.route('/joinSwarm', methods = ['GET', 'POST'])
 def joinSwarm():
     alert = ''
     if request.method == 'POST':
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        username = request.form['username']
-        fname = request.form['fname']
-        lname = request.form['lname']
-        address = request.form['address']
-        birthdate = request.form['birthdate']
-        if len(username) <= 40 and len(fname) <= 100 and len(lname) <= 100 and len(address) <= 500 and len(birthdate) == 10:
-            cursor.execute('call add_owner(% s, % s, % s, % s, % s)', (username, fname, lname, address, birthdate))
+        ip_id = request.form['ip_id']
+        ip_tag = request.form['ip_tag']
+        ip_swarm_leader_tag = request.form['ip_swarm_leader_tag']
+        if len(ip_id) <= 40:
+            cursor.execute('call join_drone(% s, % d, % d)', (ip_id, ip_tag, ip_swarm_leader_tag))
             mysql.connection.commit()
             alert = 'query executed!'
         else:
             alert = 'field lengths incorrect'
-    return render_template('addUser.html', alert = alert)
+    return render_template('joinSwarm.html', alert = alert)
 
 @app.route('/leaveSwarm', methods = ['GET', 'POST'])
 def leaveSwarm():
     alert = ''
     if request.method == 'POST':
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        username = request.form['username']
-        fname = request.form['fname']
-        lname = request.form['lname']
-        address = request.form['address']
-        birthdate = request.form['birthdate']
-        if len(username) <= 40 and len(fname) <= 100 and len(lname) <= 100 and len(address) <= 500 and len(birthdate) == 10:
-            cursor.execute('call add_owner(% s, % s, % s, % s, % s)', (username, fname, lname, address, birthdate))
+        ip_id = request.form['ip_id']
+        ip_tag = request.form['ip_tag']
+        if len(ip_id) <= 40:
+            cursor.execute('call leave_swarm(% s, % d, % d)', (ip_id, ip_tag, ip_swarm_leader_tag))
             mysql.connection.commit()
             alert = 'query executed!'
         else:
             alert = 'field lengths incorrect'
-    return render_template('addUser.html', alert = alert)
+    return render_template('leaveSwarm.html', alert = alert)
 
 @app.route('/startFunding', methods = ['GET', 'POST'])
 def startFunding():
     alert = ''
     if request.method == 'POST':
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        username = request.form['username']
-        fname = request.form['fname']
-        lname = request.form['lname']
-        address = request.form['address']
-        birthdate = request.form['birthdate']
-        if len(username) <= 40 and len(fname) <= 100 and len(lname) <= 100 and len(address) <= 500 and len(birthdate) == 10:
-            cursor.execute('call add_owner(% s, % s, % s, % s, % s)', (username, fname, lname, address, birthdate))
+        owner = request.form['ip_owner']
+        long_name = request.form['ip_long_name']
+        if len(owner) <= 40 and len(long_name) <= 40:
+            cursor.execute('call start_funding(% s, % s)', (owner, long_name))
             mysql.connection.commit()
             alert = 'query executed!'
         else:
             alert = 'field lengths incorrect'
-    return render_template('addUser.html', alert = alert)
+    return render_template('startFunding.html', alert = alert)
 
 @app.route('/takeoverDrone', methods = ['GET', 'POST'])
 def takeoverDrone():
@@ -118,17 +108,15 @@ def takeoverDrone():
     if request.method == 'POST':
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         username = request.form['username']
-        fname = request.form['fname']
-        lname = request.form['lname']
-        address = request.form['address']
-        birthdate = request.form['birthdate']
-        if len(username) <= 40 and len(fname) <= 100 and len(lname) <= 100 and len(address) <= 500 and len(birthdate) == 10:
-            cursor.execute('call add_owner(% s, % s, % s, % s, % s)', (username, fname, lname, address, birthdate))
+        id = request.form['ip_id']
+        tag = request.form['ip_tag']
+        if len(username) <= 40 and len(id) <= 40 :
+            cursor.execute('call add_owner(% s, % s, % d', (username, id, tag))
             mysql.connection.commit()
             alert = 'query executed!'
         else:
             alert = 'field lengths incorrect'
-    return render_template('addUser.html', alert = alert)
+    return render_template('takeoverDrone.html', alert = alert)
 
 @app.route('/displayEmployee')
 def displayEmployee():
